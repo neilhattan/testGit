6e3f09a63ba951dd95e21d18a52b593d3c09a1cc
neilhattan <neilhattan@users.noreply.github.com>
2023-02-12 19:40:24 +0000

    Add comments to explain functions

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 import  time
 
 def addup(n1, n2):
+    # add two arguments
     total = n1 + n2
     return total
 
 a = 3
 b = 5
-t = addup(a,b)
+total = addup(a,b)
+
+# output total using function
 print("total:",t)
 
